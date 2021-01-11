72527345f0ea68e319ed73164256bc472b7a3c94
Kaung Linn Htike <71516396+KaungLinnHtike@users.noreply.github.com>
2021-01-11 22:44:48 +0630

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+var marks = 50;
+
+if(marks >= 0 && marks < 40){
+    console.log("Fail The Exam");
+}else if(marks >= 40 && marks < 65){
+    console.log("Pass The Exam");
+}else if(marks >= 65 && marks < 80){
+    console.log("Creadit The Exam!");
+}else if(marks >= 80 && marks <= 100){
+    console.log("Outstanding!")
+}else{
+    console.log("Error!")
+}
+
